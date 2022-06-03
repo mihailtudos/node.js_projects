c9ca2905e8606a20bdff032f224ca6906ea84e77
Mihail Tudos <mihailtudos@outlook.com>
2022-06-03 14:10:09 +0100

    added template engine and dynamic views to the weatehr app

--- webserver/src/app.js
+++ webserver/src/app.js
@@ -1,29 +1,68 @@
 const express = require('express');
+const path = require('path');
+const hbs = require('hbs');
 
 const app = express();
 
-app.get('/', (req, res) => {
-	res.send({
-		name: 'Andrew',
-		yearsOld: 27
-	});
-})
+// defines paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
+// setup handlebars templet engine
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+// setup static location to serve
+app.use(express.static(publicDirectoryPath));
+
+// routes
+app.get('', (req, res) => {
+	res.render('index', {
+		title: 'Weather Node.js app',
+		author_name: "Mihail Tudos"
+	})
+})
 app.get('/help', (req, res) => {
-	res.send('Hello help page');
+	res.render('help/help', {
+		title: 'We are here to help you',
+		author_name: "Mihail Tudos",
+		description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis natus voluptates eos? Iusto aut, culpa consectetur voluptatum inventore blanditiis dolorem adipisci laboriosam. Qui ratione vitae reiciendis impedit repudiandae dicta sed ex voluptates. Consequatur illo earum inventore tenetur reprehenderit ut vel animi atque, modi ipsum eligendi placeat beatae rerum provident explicabo quaerat alias deserunt perferendis, voluptates voluptate error esse id? Hic sit nostrum beatae aperiam perferendis. Placeat temporibus quidem perspiciatis ratione deserunt. Sint eum, consequuntur ex sunt sit eveniet quidem eius, praesentium hic eos, blanditiis est odio. Vel maiores quo libero ratione laborum culpa quas cupiditate perferendis iste ipsam. Reprehenderit, animi!'
+	})
 })
 
 app.get('/about', (req, res) => {
-	res.send('Hello about page');
+	res.render('about/about', {
+		title: 'This page tells more about the author',
+		author_name: "Mihail Tudos"
+	});
 })
 
 app.get('/weather', (req, res) => {
 	res.send('Hello from wather page');
 })
 
+app.get('/help/*', (req, res) => {
+	res.render('404/404-page', {
+		title: '404',
+		message: 'Please try different route to find the article you are looking for',
+		author_name: "Mihail Tudos"
+	})
+});
+
+app.get('*', (req, res) => {
+	res.render('404/404-page', {
+		title: '404',
+		message: 'Please, click this link to return to the home page',
+		author_name: "Mihail Tudos"
+	});
+});
+
 
 const port = 3000;
 
+// starting server
 app.listen(port, () => {
 	console.log('Server is up on port ' + port);
 });
