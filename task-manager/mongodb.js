882455d9661408e9ec0d8108458123c654b52b2f
Mihail Tudos <mihailtudos@outlook.com>
2022-06-06 10:35:47 +0100

    added mongodb connection

---
+++ task-manager/mongodb.js
@@ -0,0 +1,20 @@
+const mongodb = require('mongodb');
+
+const MongoClient = mongodb.MongoClient;
+
+const connectionURL = 'mongodb://127.0.0.1:27017';
+const databaseName = 'task-manager';
+
+MongoClient.connect(connectionURL, { useNewUrlParser: true}, (error, client) => {
+	if (error) {
+		return console.log('Unable to connect to MongoDB');
+	}
+
+	// database reference
+	const db = client.db(databaseName);
+
+	db.collection('users').insertOne({
+		name: 'Mihail 2',
+		age: 22
+	})
+})
